51a7993580dd0628c166c413a3f24737c00f4559
bruce mcpherson <bruce@mcpher.com>
2021-01-27 13:52:10 +0000

    added merging

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const { queryDefinition } = require("./src/settings");
 const { fetchAllCode, decorators, gistCreate } = require("./src/fetchgit");
 
 const { cacheGet, cacheSet } = require("./src/cache");
+const writeJsonFile = require("write-json-file");
 
 const argv = require("yargs/yargs")(process.argv.slice(2)).usage(
-  "$0 -c -f -m -t -p (create,force update, max to write, test mode -dont write update page to start at"
+  "$0 -c -f -m -t -p -o (create,force update, max to write, test mode -dont write update page to start at - write file"
 ).argv;
 
 // make redis from scratchyarn ad
@@ -53,13 +54,20 @@ const getFromCache = async ({ noCache, max }) => {
 };
 
 (async () => {
-  const started = new Date().getTime()
+  const started = new Date().getTime();
+  // unusually we might need to create a new file
   if (argv.c) {
     await gistCreate();
   } else {
+    // this will get from cache, or create new data if there's no cache data or if it's forced to
     const gd = await getFromCache({ noCache: argv.f, max: argv.m });
 
+    // this not required here but it will validate the contents at least
     const mf = enumerateManifests(gd);
+
+    if (argv.o) {
+      await writeJsonFile(argv.o, gd.export());
+    }
     console.log({
       repos: gd.repos.size,
       owners: gd.owners.size,
@@ -74,7 +82,11 @@ const getFromCache = async ({ noCache, max }) => {
       oauthScopes: mf.maps.oauthScopes.size,
       dataStudios: mf.maps.dataStudios.size,
     });
-    console.log('...done after', ((new Date().getTime() - started) /1000/60).toFixed(2), 'mins')
+    console.log(
+      "...done after",
+      ((new Date().getTime() - started) / 1000 / 60).toFixed(2),
+      "mins"
+    );
     //console.log(JSON.stringify(Array.from(gd.shaxs.values()).map(f=>f)))
     /*
     console.log(Array.from(mf.maps.advancedServices));
@@ -92,6 +104,5 @@ const getFromCache = async ({ noCache, max }) => {
     console.log(mf.labels("addOns"));
     console.log(mf.labels("oauthScopes"));
     */
-
   }
 })();
